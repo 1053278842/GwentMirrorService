9b995e0939b97faf827dae2affb4499e97bfbb30
Liu Long <1053278842@qq.com>
2023-04-17 13:10:24 +0800

    修改上传页面关联增加欢迎页

---
+++ src/test/java/com/ll/gwentmirror/GwentMirrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.gwentmirror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwentMirrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
